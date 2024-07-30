5545a5a1f9f8bb3ca11122cf99c453c9f19f5fad
Evangelina Vega <evangelina1001@hotmail.com>
2024-07-29 17:01:04 -0700

    Finished exercise

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 $(function() {
   // YOUR CODE HERE
+  //EXERCISE ONE 
+  $("#toggleButton").on("click", () => {
+    $("#myParagraph").toggle()
+  })
+
+  //EXERCISE TWO
+  $("#colorButton").on("click", () => {
+    $("#colorDiv").css("background-color", "yellow")
+  })
+
+  //EXERCISE THREE
+  $("#addClassButton").on("click", () => {
+    $("#classDiv").addClass("newClass")
+  })  //its not going light blue 
+
+  $("#removeClassButton").on("click", () => {
+    $("#classDiv").removeClass("newClass")
+  })
+
+  //EXERCISE FOUR 
+  $("#fadeInButton").on("click", () => {
+    $("#fadeDiv").fadeIn()
+  })
+
+  $("#fadeOutButton").on("click", () => {
+    $(("#fadeDiv")).fadeOut()
+  })
+
+  //EXERCISE FIVE 
+  $("#slideUpButton").on("click", () => {
+    $("#slideDiv").slideUp()
+  })
+  $("#slideDownButton").on("click", () => {
+    $("#slideDiv").slideDown()
+  })
+
 })
